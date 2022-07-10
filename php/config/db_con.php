a9dbfc4242f01d8517ba4b576058c2586752ece8
miprus <44868953+miprus@users.noreply.github.com>
2022-07-10 18:36:34 +0100

    Add files via upload

--- php/config/db_con.php
+++ php/config/db_con.php
@@ -1,14 +1,8 @@
 <?php
-/*
-	$DBhost = 'db5000383480.hosting-data.io';
-	$DBuser = 'dbu8326';
-	$DBpass = 'Edinburgh123!';
-	$DBname = 'dbs369689';
-*/
-	$DBhost = 'localhost';
-	$DBuser = 'root';
-	$DBpass = 'usbw';
-	$DBname = 'fth_db';
+	$DBhost = '	nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	$DBuser = 'ylttftr9tdcyb6ph';
+	$DBpass = 'kyvedyxs8jx445f6';
+	$DBname = 'd9usszeq8gj2ls7y';
 
 	$con = mysqli_connect($DBhost, $DBuser, $DBpass, $DBname) or die("Connection error");
 ?>
