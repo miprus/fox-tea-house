875a7314a97bebaef9e5908d982dfb676dcf0480
miprus <44868953+miprus@users.noreply.github.com>
2022-07-10 18:37:07 +0100

    Add files via upload

--- php/config/db_con.php
+++ php/config/db_con.php
@@ -1,5 +1,5 @@
 <?php
-	$DBhost = '	nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	$DBhost = 'nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 	$DBuser = 'ylttftr9tdcyb6ph';
 	$DBpass = 'kyvedyxs8jx445f6';
 	$DBname = 'd9usszeq8gj2ls7y';
